e44c42d5036d3125ced83c347295f812c991d033
EstebanMarin <estebanmarin002@gmail.com>
2022-10-12 11:54:45 -0500

    finished

--- src/test/scala/streams/BloxorzSuite.scala
+++ src/test/scala/streams/BloxorzSuite.scala
@@ -83,125 +83,116 @@ class BloxorzSuite extends munit.FunSuite:
 
         
 
-  // test("Basic terrainFunction check") {
-  //   new Level1:
-  //     val in = Pos(0, 0)
-  //     val out = Pos(2, 1)
-  //     val out2 = Pos(50, -90)
-  //     val inAs = terrainFunction(mapTest)(in)
-  //     val outAs = terrainFunction(mapTest)(out)
-  //     val outAs2 = terrainFunction(mapTest)(out2)
-  //     assertEquals(inAs, true)
-  //     assertEquals(outAs, false)
-  //     assertEquals(outAs, false)
-  // }
+  test("Basic terrainFunction check") {
+    new Level1:
+      val in = Pos(0, 0)
+      val out = Pos(2, 1)
+      val out2 = Pos(50, -90)
+      val inAs = terrainFunction(mapTest)(in)
+      val outAs = terrainFunction(mapTest)(out)
+      val outAs2 = terrainFunction(mapTest)(out2)
+      assertEquals(inAs, true)
+      assertEquals(outAs, false)
+      assertEquals(outAs, false)
+  }
 
-  // test("testing find Char with bigger terrain") {
-  //   new Level1:
-  //     val in1 = Pos(1, 1)
-  //     val in2 = Pos(4, 7)
-  //     val out1 = Pos(5, 2)
-  //     val out2 = Pos(50, 2)
-  //     val in1T = terrain(in1)
-  //     val in2T = terrain(in2)
-  //     val out1T = terrain(out1)
-  //     val out2T = terrain(out2)
-  //     assertEquals(in1T, true)
-  //     assertEquals(in2T, true)
-  //     assertEquals(out1T, false)
-  //     assertEquals(out2T, false)
-  // }
+  test("testing find Char with bigger terrain") {
+    new Level1:
+      val in1 = Pos(1, 1)
+      val in2 = Pos(4, 7)
+      val out1 = Pos(5, 2)
+      val out2 = Pos(50, 2)
+      val in1T = terrain(in1)
+      val in2T = terrain(in2)
+      val out1T = terrain(out1)
+      val out2T = terrain(out2)
+      assertEquals(in1T, true)
+      assertEquals(in2T, true)
+      assertEquals(out1T, false)
+      assertEquals(out2T, false)
+  }
 
-  // test("terrain function level 1 (10pts)") {
-  //   new Level1:
-  //     assert(terrain(Pos(0, 0)), "0,0")
-  //     assert(terrain(Pos(1, 1)), "1,1") // start
-  //     assert(terrain(Pos(4, 7)), "4,7") // goal
-  //     assert(terrain(Pos(5, 8)), "5,8")
-  //     assert(!terrain(Pos(5, 9)), "5,9")
-  //     assert(terrain(Pos(4, 9)), "4,9")
-  //     assert(!terrain(Pos(6, 8)), "6,8")
-  //     assert(!terrain(Pos(4, 11)), "4,11")
-  //     assert(!terrain(Pos(-1, 0)), "-1,0")
-  //     assert(!terrain(Pos(0, -1)), "0,-1")
-  // }
+  test("terrain function level 1 (10pts)") {
+    new Level1:
+      assert(terrain(Pos(0, 0)), "0,0")
+      assert(terrain(Pos(1, 1)), "1,1") // start
+      assert(terrain(Pos(4, 7)), "4,7") // goal
+      assert(terrain(Pos(5, 8)), "5,8")
+      assert(!terrain(Pos(5, 9)), "5,9")
+      assert(terrain(Pos(4, 9)), "4,9")
+      assert(!terrain(Pos(6, 8)), "6,8")
+      assert(!terrain(Pos(4, 11)), "4,11")
+      assert(!terrain(Pos(-1, 0)), "-1,0")
+      assert(!terrain(Pos(0, -1)), "0,-1")
+  }
 
-  // test("find char level 1 (10pts)") {
-  //   new Level1:
-  //     assertEquals(startPos, Pos(1, 1))
-  // }
+  test("find char level 1 (10pts)") {
+    new Level1:
+      assertEquals(startPos, Pos(1, 1))
+  }
 
-  // test("Test Block isStanding") {
-  //   new Level1:
-  //     val block1 = Pos(0, 0)
-  //     val block2 = Pos(0, 0)
-  //     val block3 = Pos(0, 1)
-  //     val blockUp = Block(block1, block2)
-  //     val blockDown = Block(block1, block3)
-  //     assertEquals(blockUp.isStanding, true)
-  //     assertEquals(blockDown.isStanding, false)
-  // }
+  test("Test Block isStanding") {
+    new Level1:
+      val block1 = Pos(0, 0)
+      val block2 = Pos(0, 0)
+      val block3 = Pos(0, 1)
+      val blockUp = Block(block1, block2)
+      val blockDown = Block(block1, block3)
+      assertEquals(blockUp.isStanding, true)
+      assertEquals(blockDown.isStanding, false)
+  }
 
-  // test("Test Legal positions") {
-  //   new Level1:
-  //     val block1 = Pos(0, 0)
-  //     val block2 = Pos(0, 0)
-  //     val block3 = Pos(0, 4)
-  //     val block4 = Pos(0, 5)
-  //     val blockLegal = Block(block1, block2)
-  //     val blockiLegal = Block(block3, block4)
-  //     assertEquals(blockLegal.isLegal, true)
-  //     assertEquals(blockiLegal.isLegal, false)
-  // }
+  test("Test Legal positions") {
+    new Level1:
+      val block1 = Pos(0, 0)
+      val block2 = Pos(0, 0)
+      val block3 = Pos(0, 4)
+      val block4 = Pos(0, 5)
+      val blockLegal = Block(block1, block2)
+      val blockiLegal = Block(block3, block4)
+      assertEquals(blockLegal.isLegal, true)
+      assertEquals(blockiLegal.isLegal, false)
+  }
 
-  // test("neighbors legal") {
-  //   new Level1:
-  //     val block1 = Pos(0, 0)
-  //     val block2 = Pos(0, 0)
-  //     val block3 = Pos(0, 4)
-  //     val block4 = Pos(0, 5)
-  //     val blockLegal = Block(block1, block2).legalNeighbors
-  //     val legalOption = List(
-  //       (Block(Pos(0, 1), Pos(0, 2)), Move.Right),
-  //       (Block(Pos(1, 0), Pos(2, 0)), Move.Down)
-  //     )
-  //     val blockiLegal = Block(block3, block4).legalNeighbors
-  //     assertEquals(blockiLegal, Nil)
-  //     assertEquals(blockLegal, legalOption)
-  // }
+  test("neighbors legal") {
+    new Level1:
+      val block1 = Pos(0, 0)
+      val block2 = Pos(0, 0)
+      val block3 = Pos(0, 4)
+      val block4 = Pos(0, 5)
+      val blockLegal = Block(block1, block2).legalNeighbors
+      val legalOption = List(
+        (Block(Pos(0, 1), Pos(0, 2)), Move.Right),
+        (Block(Pos(1, 0), Pos(2, 0)), Move.Down)
+      )
+      val blockiLegal = Block(block3, block4).legalNeighbors
+      assertEquals(blockiLegal, Nil)
+      assertEquals(blockLegal, legalOption)
+  }
 
-  // test("neighbors with history test and finished") {
-  //   new Level0:
-  //     val blockLegal = startBlock
-  //     val test = neighborsWithHistory(blockLegal, Nil).head
-  //     val otherLegalMove =
-  //       neighborsWithHistory(Block(Pos(2, 2), Pos(3, 2)), List(Move.Down))
-  //     val legalMove = (Block(Pos(2, 2), Pos(3, 2)), List(Move.Down))
-  //     val finishedMov =
-  //       (Block(Pos(4, 2), Pos(4, 2)), List(Move.Down, Move.Down))
-  //     assertEquals(test, legalMove)
-  //     assertEquals(done(otherLegalMove.tail.head._1), true)
-  //     assertEquals(otherLegalMove.tail.head, finishedMov)
-  // }
+  test("neighbors with history test and finished") {
+    new Level0:
+      val blockLegal = startBlock
+      val test = neighborsWithHistory(blockLegal, Nil).head
+      val otherLegalMove =
+        neighborsWithHistory(Block(Pos(2, 2), Pos(3, 2)), List(Move.Down))
+      val legalMove = (Block(Pos(2, 2), Pos(3, 2)), List(Move.Down))
+      val finishedMov =
+        (Block(Pos(4, 2), Pos(4, 2)), List(Move.Down, Move.Down))
+      assertEquals(test, legalMove)
+      assertEquals(done(otherLegalMove.tail.head._1), true)
+      assertEquals(otherLegalMove.tail.head, finishedMov)
+  }
 
-  // test("testing web site case neighbors with history") {
-  //   new Level1:
-  //     val blockLegal = startBlock
-  //     val test = neighborsWithHistory(
-  //       Block(Pos(1, 1), Pos(1, 1)),
-  //       List(Move.Left, Move.Up)
-  //     )
-  //     assertEquals(test.head._2, List(Move.Right, Move.Left, Move.Up))
-  // }
 
-  // test("test new neighBours only") {
-  //   new Level0:
-  //     val blockLegal = startBlock
-  //     val history = List.empty
-  //     val neighborsStream = neighborsWithHistory(blockLegal, history)
-  //     val test = (Block(Pos(2, 2), Pos(3, 2)), List(Move.Down))
-  //     assertEquals(test._2(0), test._2(0))
-  // }
+  test("test new neighBours only") {
+    new Level0:
+      val blockLegal = startBlock
+      val history = List.empty
+      val neighborsStream = neighborsWithHistory(blockLegal, history)
+      val test = (Block(Pos(2, 2), Pos(3, 2)), List(Move.Down))
+      assertEquals(test._2(0), test._2(0))
+  }
 
   test("new Neightbors only web test") {
     new Level1:
